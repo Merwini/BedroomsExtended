732095fc230d01a46c6983fb2cf3c6e79b160654
Merwini <86023141+Merwini@users.noreply.github.com>
2023-07-09 10:47:49 -0400

    made class for RoomDesireWorker

---
+++ RimWorld Template1/RoomDesireWorkers/RoomDesireWorker.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nuff.PersonalizedBedrooms
+{
+    public class RoomDesireWorker
+    {
+        public RoomDesire parent;
+
+        public RoomDesireWorker(RoomDesire parent)
+        {
+            this.parent = parent;
+        }
+    }
+}
