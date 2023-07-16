bf377053c695c5e3481e1ddc8ebd86059c9a17b8
Merwini <86023141+Merwini@users.noreply.github.com>
2023-07-16 12:46:43 -0400

    Hopefully modified the Window_RoomDesire to have an info icon next to each desire that can be moused over to display the more detailed description

---
+++ RimWorld Template1/Window_DesireDescription.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using Verse;
+
+
+namespace nuff.PersonalizedBedrooms
+{
+    public class Window_DesireDescription : Window
+    {
+        private string additionalInfo;
+
+        public Window_DesireDescription(string additionalInfo)
+        {
+            this.additionalInfo = additionalInfo;
+            forcePause = true;
+            absorbInputAroundWindow = true;
+            closeOnClickedOutside = true;
+            draggable = false;
+            doCloseButton = false;
+            doCloseX = false;
+            preventCameraMotion = false;
+            soundAppear = null;
+            soundClose = null;
+            onlyOneOfTypeAllowed = false;
+            closeOnAccept = false;
+        }
+
+        public override Vector2 InitialSize
+        {
+            get
+            {
+                Vector2 textSize = Text.CalcSize(additionalInfo);
+                return new Vector2(textSize.x + 20f, textSize.y + 20f);
+            }
+        }
+
+        public override void DoWindowContents(Rect inRect)
+        {
+            Widgets.DrawBoxSolid(inRect, Color.black);
+            Text.Anchor = TextAnchor.MiddleCenter;
+            GUI.color = Color.white;
+            Widgets.Label(inRect, additionalInfo);
+        }
+    }
+}
--- RimWorld Template1/Window_RoomDesire.cs
+++ RimWorld Template1/Window_RoomDesire.cs
@@ -67,6 +67,83 @@ namespace nuff.PersonalizedBedrooms
                 foreach (KeyValuePair<RoomDesire, bool> entry in dictList[i])
                 {
                     displayDesiresTotal++;
+                    if (entry.Value)
+                        displayDesiresMet++;
+                }
+                if (displayDesiresMet >= desiresNeeded)
+                {
+                    displayTierStatus = "ACTIVE";
+                    displayTierColor = "<color=green>";
+                }
+                else
+                {
+                    displayTierStatus = "INACTIVE";
+                    displayTierColor = "<color=red>";
+                }
+                string tierLabel = $"<color=white>Tier {desireTierStrings[i]} desires. ({displayDesiresMet.ToString()}/{displayDesiresTotal.ToString()} met. Status: </color>{displayTierColor}{displayTierStatus}</color>";
+                list.Label(tierLabel);
+
+                foreach (KeyValuePair<RoomDesire, bool> entry in dictList[i])
+                {
+                    if (entry.Value)
+                    {
+                        GUI.color = Color.green;
+                    }
+                    else
+                    {
+                        GUI.color = Color.red;
+                    }
+
+                    // Draw the label
+                    Rect labelRect = list.GetRect(Verse.Text.LineHeight);
+                    Rect iconRect = new Rect(labelRect.xMax - 30f, labelRect.y, 24f, 24f);
+
+                    // Draw the icon
+                    Texture2D iconTexture = ContentFinder<Texture2D>.Get("UI/Icons/Info");
+                    Widgets.Label(labelRect, entry.Key.label);
+                    if (Mouse.IsOver(iconRect) && Event.current.type == EventType.Repaint)
+                    {
+                        GUI.DrawTexture(iconRect, iconTexture);
+                        if (Widgets.ButtonInvisible(iconRect))
+                        {
+                            // Handle icon click here
+                            string additionalInfo = entry.Key.def.description;
+                            Find.WindowStack.Add(new Window_DesireDescription(additionalInfo));
+                        }
+                    }
+
+                    list.Gap(list.verticalSpacing);
+                }
+            }
+        }
+
+        public override void Close(bool doCloseSound = true)
+        {
+            base.Close(doCloseSound);
+            //TODO I forgot what I want this to do
+        }
+
+
+        /*
+        public override void DoWindowContents(Rect inRect)
+        {
+            int displayDesiresTotal = 0;
+            int displayDesiresMet = 0;
+            string displayTierStatus = "ERROR";
+            string displayTierColor = "ERROR";
+
+            Listing_Standard list = new Listing_Standard();
+            list.Begin(inRect);
+            Verse.Text.Font = GameFont.Medium;
+            GUI.color = Color.white;
+            list.Label("Bedroom Desires for " + pawn.Name);
+            Verse.Text.Font = GameFont.Small;
+
+            for (int i = 0; i < dictList.Count; i++)
+            {
+                foreach (KeyValuePair<RoomDesire, bool> entry in dictList[i])
+                {
+                    displayDesiresTotal++;
                     if (entry.Value == true)
                         displayDesiresMet++;
                 }
@@ -98,11 +175,6 @@ namespace nuff.PersonalizedBedrooms
                 }
             }
         }
-
-        public override void Close(bool doCloseSound = true)
-        {
-            base.Close(doCloseSound);
-            //TODO I forgot what I want this to do
-        }
+         */
     }
 }
