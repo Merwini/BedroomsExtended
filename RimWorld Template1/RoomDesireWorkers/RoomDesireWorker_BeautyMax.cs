6aa122eb599e5cb5bc59167a04ab8cce19852633
Merwini <86023141+Merwini@users.noreply.github.com>
2023-07-19 13:46:04 -0400

    Implemented RoomDesireWorker_BeautyMax and made associated defs

---
+++ RimWorld Template1/RoomDesireWorkers/RoomDesireWorker_BeautyMax.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+using RimWorld;
+
+namespace nuff.PersonalizedBedrooms.RoomDesireWorkers
+{
+    class RoomDesireWorker_BeautyMax : RoomDesireWorker
+    {
+        int minimumBeauty;
+        public RoomDesireWorker_BeautyMax(RoomDesire parent) : base(parent)
+        {
+            minimumBeauty = parent.def.minimumQuantity;
+        }
+
+        public override bool IsMet(Pawn pawn, Room room)
+        {
+            bool isMet = false;
+            foreach (Thing thing in room.ContainedAndAdjacentThings)
+            {
+                if (thing is Building building && building.GetStatValue(StatDefOf.Beauty) >= minimumBeauty)
+                {
+                    isMet = true;
+                    break;
+                }
+            }
+
+            return isMet;
+        }
+    }
+}
