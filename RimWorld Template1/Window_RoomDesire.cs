4d98da1d16bf4028317bfb0229ce131c83c36902
Merwini <86023141+Merwini@users.noreply.github.com>
2023-07-29 12:07:53 -0400

    Changed desire description tooltip to just appearing over the label, instead of its own button

--- RimWorld Template1/Window_RoomDesire.cs
+++ RimWorld Template1/Window_RoomDesire.cs
@@ -22,6 +22,10 @@ namespace nuff.PersonalizedBedrooms
 
         int desiresNeeded;
 
+        string colorRed = "<color=#ff3333>";
+        string colorGreen = "<color=#15b01a>";
+        string colorYellow = "<color=#ffff14>";
+
 
         public enum DesireTierStatus
         {
@@ -77,6 +81,7 @@ namespace nuff.PersonalizedBedrooms
             if (room != null)
             {
                 Verse.Text.Font = GameFont.Small;
+                bool stillAvailable = true;
                 for (int i = 0; i < dictList.Count; i++)
                 {
                     displayDesiresTotal = 0;
@@ -88,46 +93,53 @@ namespace nuff.PersonalizedBedrooms
                         if (entry.Value)
                             displayDesiresMet++;
                     }
-                    if (displayDesiresMet >= desiresNeeded)
+                    if (stillAvailable)
                     {
-                        displayTierStatus = "ACTIVE";
-                        displayTierColor = "<color=green>";
+                        if (displayDesiresMet >= desiresNeeded)
+                        {
+                            displayTierStatus = "ACTIVE";
+                            displayTierColor = colorGreen;
+                        }
+                        else
+                        {
+                            displayTierStatus = "INACTIVE";
+                            displayTierColor = colorRed;
+                            stillAvailable = false;
+                        }
                     }
                     else
                     {
-                        displayTierStatus = "INACTIVE";
-                        displayTierColor = "<color=red>";
+                        displayTierStatus = "UNAVAILABLE";
+                        displayTierColor = colorYellow;
                     }
+
                     string tierLabel = $"<color=white>Tier {desireTierStrings[i]} desires. ({displayDesiresMet.ToString()}/{displayDesiresTotal.ToString()} met. Status: </color>{displayTierColor}{displayTierStatus}</color>";
                     list.Label(tierLabel);
                     foreach (KeyValuePair<RoomDesire, bool> entry in dictList[i])
                     {
+                        Color color;
                         if (entry.Value)
                         {
-                            GUI.color = Color.green;
+                            //color = new Color(0.055f, 0.541f, 0.086f);
+                            color = ColorLibrary.Green;
                         }
                         else
                         {
-                            GUI.color = Color.red;
+                            //color = new Color(0.714f, 0.008f, 0.020f);
+                            color = ColorLibrary.RedReadable;
                         }
+                        GUI.color = color;
 
                         // Draw the label
                         Rect labelRect = list.GetRect(Verse.Text.LineHeight);
-                        Rect iconRect = new Rect(labelRect.xMax - 30f, labelRect.y, 24f, 24f);
+                        //Rect iconRect = new Rect(labelRect.xMax - 30f, labelRect.y, 24f, 24f);
                         Widgets.Label(labelRect, entry.Key.label);
 
                         // Draw the icon
-                        Texture2D iconTexture = ContentFinder<Texture2D>.Get("UI/Icons/QuestionMark");
-                        GUI.DrawTexture(iconRect, iconTexture);
-                        TooltipHandler.TipRegion(iconRect, entry.Key.def.description);
-                        /*
-                        if (Widgets.ButtonImage(iconRect, iconTexture))
-                        {
-                            // Handle button click here
-                            string additionalInfo = entry.Key.def.description;
-                            Find.WindowStack.Add(new Window_DesireDescription(additionalInfo));
-                        }
-                        */
+                        //Texture2D iconTexture = ContentFinder<Texture2D>.Get("UI/Icons/QuestionMark");
+                        //GUI.DrawTexture(iconRect, iconTexture);
+                        TooltipHandler.TipRegion(labelRect, entry.Key.def.description);
+
 
                         list.Gap(list.verticalSpacing);
                     }
@@ -152,59 +164,5 @@ namespace nuff.PersonalizedBedrooms
             base.Close(doCloseSound);
             //TODO I forgot what I want this to do
         }
-
-
-        /*
-        public override void DoWindowContents(Rect inRect)
-        {
-            int displayDesiresTotal = 0;
-            int displayDesiresMet = 0;
-            string displayTierStatus = "ERROR";
-            string displayTierColor = "ERROR";
-
-            Listing_Standard list = new Listing_Standard();
-            list.Begin(inRect);
-            Verse.Text.Font = GameFont.Medium;
-            GUI.color = Color.white;
-            list.Label("Bedroom Desires for " + pawn.Name);
-            Verse.Text.Font = GameFont.Small;
-
-            for (int i = 0; i < dictList.Count; i++)
-            {
-                foreach (KeyValuePair<RoomDesire, bool> entry in dictList[i])
-                {
-                    displayDesiresTotal++;
-                    if (entry.Value == true)
-                        displayDesiresMet++;
-                }
-                if (displayDesiresMet >= desiresNeeded)
-                {
-                    displayTierStatus = "ACTIVE";
-                    displayTierColor = "<color=green>";
-                }
-                else
-                {
-                    displayTierStatus = "INACTIVE";
-                    displayTierColor = "<color=red>";
-                }
-                string tierLabel = $"<color=white>Tier {desireTierStrings[i]} desires. ({displayDesiresMet.ToString()}/{displayDesiresTotal.ToString()} met. Status: </color>{displayTierColor}{displayTierStatus}</color>";
-                list.Label(tierLabel);
-
-                foreach (KeyValuePair<RoomDesire, bool> entry in dictList[i])
-                {
-                    //TODO
-                    if (entry.Value == true)
-                    {
-                        GUI.color = Color.green;
-                    }
-                    else
-                    {
-                        GUI.color = Color.red;
-                    }
-                    list.Label(entry.Key.label);
-                }
-            }
-        }
-         */
     }
 }
